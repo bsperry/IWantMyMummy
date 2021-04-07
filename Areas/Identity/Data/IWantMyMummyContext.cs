e797d92e03a79540716ce9812463e100f2f5b2c1
bsperry <69691639+bsperry@users.noreply.github.com>
2021-04-07 15:30:23 -0600

    Add project files.

---
+++ Areas/Identity/Data/IWantMyMummyContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IWantMyMummy.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IWantMyMummy.Data
+{
+    public class IWantMyMummyContext : IdentityDbContext<IWantMyMummyUser>
+    {
+        public IWantMyMummyContext(DbContextOptions<IWantMyMummyContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using IWantMyMummy.Areas.Identity.Data;
+using IWantMyMummy.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IWantMyMummy.Areas.Identity.IdentityHostingStartup))]
+namespace IWantMyMummy.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IWantMyMummyContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IWantMyMummyContextConnection")));
+
+                services.AddDefaultIdentity<IWantMyMummyUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IWantMyMummyContext>();
+            });
+        }
+    }
+}
