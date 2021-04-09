4aac2ec584012cd46c23f486db05c006da89f854
ohsha <ohsha@DESKTOP-677NSFV>
2021-04-09 16:23:48 -0600

    Create PageNumberingInfo.cs

---
+++ Models/ViewModels/PageNumberingInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IWantMyMummy.Models.ViewModels
+{
+    public class PageNumberingInfo 
+    {
+        public int NumItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalNumItems { get; set; }
+
+        //calculate num pages
+
+        public int NumPages => (int)(Math.Ceiling((decimal)TotalNumItems / NumItemsPerPage));
+    }
+}
