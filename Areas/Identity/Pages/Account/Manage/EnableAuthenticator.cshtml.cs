5c921349eadf0a72f8c2715a1d4ebcc6e1f1320e
bsperry <69691639+bsperry@users.noreply.github.com>
2021-04-09 18:42:30 -0600

    Update EnableAuthenticator.cshtml.cs

--- Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -20,7 +20,7 @@ namespace IWantMyMummy.Areas.Identity.Pages.Account.Manage
         private readonly ILogger<EnableAuthenticatorModel> _logger;
         private readonly UrlEncoder _urlEncoder;
 
-        private const string AuthenticatorUriFormat = "otpauth://totp/{0}:{1}secret={2}&issuer={0}&digits=6";
+        private const string AuthenticatorUriFormat = "otpauth://totp/{0}:{1}?secret={2}&issuer={0}&digits=6";
 
         public EnableAuthenticatorModel(
             UserManager<IWantMyMummyUser> userManager,
