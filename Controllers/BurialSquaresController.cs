d0404f2bbaa431a515cf4e359226cb5c1f0a8a6c
bsperry <69691639+bsperry@users.noreply.github.com>
2021-04-09 18:43:18 -0600

    Update BurialSquaresController.cs

--- Controllers/BurialSquaresController.cs
+++ Controllers/BurialSquaresController.cs
@@ -51,7 +51,7 @@ namespace IWantMyMummy.Controllers
 
         // POST: BurialSquares/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/LinkId=317598.
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("BurialSquareId,BurialLocationNs,BurialLocationEw,LowPairNs,HighPairNs,LowPairEw,HighPairEw")] BurialSquare burialSquare)
